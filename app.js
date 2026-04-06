56af9e909202e7af44fdf9324964da2f78c69bc3
DURGA DEVI R <175391763+durgadevi77r@users.noreply.github.com>
2026-04-06 09:52:59 +0530

    modifing the app.js
    Removed the root route handler that sends a greeting message.

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello from CI/CD Pipeline 🚀');
-});
+app.use(express.static('public'));
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
